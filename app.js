5febf4997df0e533887d7fa363a2ac8dfe2befb8
Krish Kumar <krishkumar3004@gmail.com>
2023-04-14 21:44:54 +0530

    removed node_modules and added .lues and added .gitignore

--- app.js
+++ app.js
@@ -1,65 +1,66 @@
 const express = require("express");
 const path = require("path");
-const ejsMate = require('ejs-mate');
-const ExpressError = require('./utils/ExpressError')
-const Joi = require('joi');
-const methodOverride = require('method-override');
-const session = require('express-session');
-const flash = require('connect-flash');
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError");
+const Joi = require("joi");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const mongoose = require("mongoose");
-mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://127.0.0.1:27017/medimatch', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true})
-.then(() =>{
+mongoose.set("strictQuery", true);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/medimatch", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("Connection open !");
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log("Oh No! Error :(");
-    console.log(err); 
-} )
-  
+    console.log(err);
+  });
+
 const app = express();
 
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-const doctors = require('./routes/doctor');
-const appointments = require('./routes/appointments');
+const doctors = require("./routes/doctor");
+const appointments = require("./routes/appointments");
 
-
-app.use(express.urlencoded({extended : true}));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 const sessionConfig = {
-  secret : 'thisisasecret',
+  secret: "thisisasecret",
   resave: false,
   saveUninitialized: true,
-  cookie:{
-    httpOnly:true,
-    expires: Date.now() + 1000*60*60*24*7,
-    maxAge: 1000*60*60*24*7
-  }
-}
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
 app.use(session(sessionConfig));
 app.use(flash());
 
-app.use((req, res, next) =>{
-  res.locals.success = req.flash('success');
-  res.locals.error = req.flash('error');
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
   next();
-})
+});
 
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
   res.redirect("index");
 });
 
-//----------------importing routes------------------ 
-app.use('/doctors', doctors);
-app.use('/doctors/:id/appointments', require('./routes/appointments'));
+//----------------importing routes------------------
+app.use("/doctors", doctors);
+app.use("/doctors/:id/appointments", require("./routes/appointments"));
 //------------------Simple Routes------------------
 app.get("/index", (req, res) => {
   res.render("index");
@@ -71,17 +72,18 @@ app.get("/gallery", (req, res) => {
   res.render("gallery");
 });
 //------------------error handling------------------
-app.all('*', (req, res, next) => {
-  next(new ExpressError('Page not found !', 404));
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page not found !", 404));
 });
 app.use((err, req, res, next) => {
-  const {statusCode = 500} = err;
-  if(!err.message) { err.message = "Oh No, Something went wrong !!!"};
-  res.status(statusCode).render('error', {err});
+  const { statusCode = 500 } = err;
+  if (!err.message) {
+    err.message = "Oh No, Something went wrong !!!";
+  }
+  res.status(statusCode).render("error", { err });
 });
 
 // -------------listening--------------------------
-app.listen("3000", (req, res) =>{
-    console.log("Listening in port 3000...");
+app.listen(process.env.PORT || "3000", (req, res) => {
+  console.log("Listening in port 3000...");
 });
-
