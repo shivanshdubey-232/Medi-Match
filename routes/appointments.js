309f5f67bba9adc2042f6fa1e6423168ff443ef7
Krish Kumar <krishkumar3004@gmail.com>
2023-05-20 18:22:10 +0530

    Added Basic Authentication

--- routes/appointments.js
+++ routes/appointments.js
@@ -1,41 +1,58 @@
-const express = require('express');
-const router = express.Router({mergeParams: true});
-const catchAsync = require('../utils/catchAsync');
-const Doctor = require('../models/doctor');
-const Appointment = require('../models/appointment');
-const {appointmentSchema} = require('../schemas.js');
-
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const catchAsync = require("../utils/catchAsync");
+const Doctor = require("../models/doctor");
+const Appointment = require("../models/appointment");
+const { appointmentSchema } = require("../schemas.js");
 
 const validateappointment = (req, res, next) => {
   console.log(req.body);
-  const {error} = appointmentSchema.validate(req.body);
-  if(error){
-    const msg = error.details.map(el => el.message);
+  const { error } = appointmentSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message);
     throw new ExpressError(msg, 400);
-  }else{
+  } else {
     console.log(req.body);
     next();
   }
-}
+};
+
+const isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl;
+    req.flash("error", "You must be signed in first !");
+    return res.redirect("/login");
+  }
+  next();
+};
 
 // ----------------------appointment route-----------------
-router.post('/', catchAsync(async(req, res) => {
-  const doctor = await Doctor.findById(req.params.id);
-  const appointment = new Appointment(req.body.appointment);
-  console.log(appointment);
-  doctor.appointments.push(appointment);
-  await appointment.save();
-  await doctor.save();
-  req.flash('success', 'Successfully created a new appointment !');
-  res.redirect(`/doctors/${doctor._id}`);
-}))
+router.post(
+  "/",
+  isLoggedIn,
+  catchAsync(async (req, res) => {
+    const doctor = await Doctor.findById(req.params.id);
+    const appointment = new Appointment(req.body.appointment);
+    console.log(appointment);
+    doctor.appointments.push(appointment);
+    await appointment.save();
+    await doctor.save();
+    req.flash("success", "Successfully created a new appointment !");
+    res.redirect(`/doctors/${doctor._id}`);
+  })
+);
 //-----------------------delete-appointment----------------
-router.delete('/:appointmentId', catchAsync(async (req, res) => {
-  await Appointment.findByIdAndDelete(req.params.appointmentId);
-  await Doctor.findByIdAndUpdate(req.params.id, {$pull: {appointments: req.params.appointmentId}});
-  req.flash('success', 'Successfully deleted an appointment !');
-  res.redirect(`/doctors/${req.params.id}`);
-}));
-
+router.delete(
+  "/:appointmentId",
+  isLoggedIn,
+  catchAsync(async (req, res) => {
+    await Appointment.findByIdAndDelete(req.params.appointmentId);
+    await Doctor.findByIdAndUpdate(req.params.id, {
+      $pull: { appointments: req.params.appointmentId },
+    });
+    req.flash("success", "Successfully deleted an appointment !");
+    res.redirect(`/doctors/${req.params.id}`);
+  })
+);
 
 module.exports = router;
